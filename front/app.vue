59a43f4fb8633c56d6c73b5410766f60f7497264
Dusan Ivancic <dusan.ivancic@otto.de>
2025-04-04 12:09:52 +0200

    di | add front

---
+++ front/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-container">
+    <header class="app-header">
+      <img src="./assets/img/logo.jpeg" alt="Logo" class="logo" />
+    </header>
+    <UApp>
+      <NuxtPage />
+    </UApp>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
