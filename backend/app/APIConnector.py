331e0a20411a7d9c68d09bafe452a9ceb8b23e48
Cyph0rW4ve <Bastiprivate1686@gmail.com>
2025-04-08 22:44:32 +0200

    fix pylint pipeline

--- backend/app/APIConnector.py
+++
@@ -1,9 +0,0 @@
-import os
-import googlemaps
-
-api_key = os.environ.get("GOOGLE_API")
-
-gmaps = googlemaps.Client(key=api_key)
-
-route = gmaps.directions("Hamburg,Germany" , "Berlin, Germany")
-print(route)
--- backend/app/fuel_consumption_calculator.py
+++ backend/app/fuel_consumption_calculator.py
@@ -1,9 +1,6 @@
 import re
 
 class FuelConsumptionCalculator:
-
-
-
     def calculate_stops(self, total_distance, fuel_tank, consumption):
         truck_range = self.get_truck_range(fuel_tank, consumption)
         filtered_distance = float(re.search(r"\d{1,3}(?:,\d{3})*(?:\.\d+)?|\d+(?:\.\d+)?", total_distance).group().replace(",", ""))
