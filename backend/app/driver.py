7daad8b001a09c8b774baf1a2aa824fdd257e181
Dusan Ivancic <dusan.ivancic@otto.de>
2025-04-08 23:28:28 +0200

    di | dev pip

--- backend/app/driver.py
+++ backend/app/driver.py
@@ -1,12 +1,9 @@
-from datetime import datetime
-
-
 class Driver:
     def __init__(self):
-        self.id = "D001"
-        self.free_drive_time = 32400
+        self.id: str = "D001"
+        self.free_drive_time: int = 32400
         self.open_break_duration: int = 0
-        self.available_for_next_job = None
+        self.available_for_next_job: bool = None
 
     def read_stats(self):
         pass
@@ -14,17 +11,17 @@ class Driver:
     def update_stats_to_db(self):
         pass
 
-    def new_driver(self, new_id):
+    def new_driver(self, new_id: str):
         pass
 
     def update_free_drive_time(self, seconds):
         self.free_drive_time -= seconds
 
     def small_break(self):
-        self.open_break_duration += 45 * 60           # 45 min in seconds
+        self.open_break_duration += 45 * 60# 45 min in seconds
 
     def big_break(self):
-        self.open_break_duration += 11 * 60 * 60      # 11 hours in seconds
+        self.open_break_duration += 11 * 60 * 60# 11 hours in seconds
 
     def get_remaining_drive_time(self):
         return self.free_drive_time
